f156e40d91bb6bdd8ab6a30a5f1ac47b55727288
Jakob Drusany <drusany.jakob@gmail.com>
2026-05-22 15:08:34 +0200

    allow mpi to spawn more tasks

--- mpi/run_lenia.sh
+++ mpi/run_lenia.sh
@@ -3,14 +3,13 @@
 NUM_PROC=$1
 GRID_SIZE=$2
 OUT_FILE=$3
-CPU_PER_TASK=$4
 
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --reservation=fri
 #SBATCH --job-name=lenia_2_${NUM_PROC}_${GRID_SIZE}
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=${CPU_PER_TASK}
+#SBATCH --ntasks=${NUM_PROC}
+#SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
 #SBATCH --nodes=1
 #SBATCH --output=${OUT_FILE}
