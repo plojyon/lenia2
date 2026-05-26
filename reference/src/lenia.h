6d3f8f27993e214e35fa414ca8fe8edfbeaabaa7
Jakob Drusany <drusany.jakob@gmail.com>
2026-05-26 16:06:54 +0200

    fix

--- reference/src/lenia.h
+++ reference/src/lenia.h
@@ -3,15 +3,13 @@
 
 #include <mpi.h>
 
-struct orbium_coo { 
+struct orbium_coo
+{
     int row;
     int col;
     int angle;
 };
 
-double *evolve_lenia(const unsigned int rows, const unsigned int cols, const unsigned int steps, const double dt, const unsigned int kernel_size, const struct orbium_coo *orbiums, const unsigned int num_orbiums);
+double *evolve_lenia(unsigned int rows, unsigned int cols, const unsigned int steps, const double dt, const unsigned int kernel_size, struct orbium_coo *orbiums, const unsigned int num_orbiums);
 
-
-#endif 
-
-
+#endif
